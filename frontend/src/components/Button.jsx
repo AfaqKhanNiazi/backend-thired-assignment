539b1b3019e68fc140307d8a6923eaae38c0d313
RehmanKhan007 <156161992+RehmanKhan007@users.noreply.github.com>
2025-01-25 12:12:43 +0500

    fronten remove

--- frontend/src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-
-export default function Button(props) {
-  return (
-    <button className={`font-medium rounded-xl bg-primary center text-white ${props.className}`}>
-        {props.children}
-
-</button>
-  )
-}
